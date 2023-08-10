18b0a976f15d08ba08b4b6db187f7bb8d751bcff
Sebastian Ponce <poncegramming@gmail.com>
2023-08-10 08:13:35 -0600

    primer dia

---
+++ prog1/10-08-2023/basics.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("esto está en vim xd")
+print(2+2) # suma
+print(2*3) # multiplicación
+print("hola " + "cómo estás") # concatenación
+print("hola "*3) # conc. con multiplicación
+
+## ASIGNACIÓN DE VARIABLES
+x = 3
+y = 9
+nombre = "Sebastian"
+
+### se pueden realizar las mismas operaciones
+print(x + y)
+print(x * y)
+print("Hola " + nombre)
---
+++ prog1/10-08-2023/firstprogram.py
@@ -0,0 +1,7 @@
+print("Cómo te llamas?")
+usuario = input()
+print("Qué tal, " + usuario)
+print("Cuántos años tienes?")
+edad = input()
+print("En 1 año tendrás " + str(int(edad) + 1) + " años.")
+print("Tu nombre tiene una longitud de " + str(len(usuario)) + " caracteres.")
