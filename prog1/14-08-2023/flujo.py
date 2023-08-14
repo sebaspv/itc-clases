09ac5e08e40dfecf6e16598c8352239728aa4f55
Sebastian Ponce <poncegramming@gmail.com>
2023-08-14 08:49:18 -0600

    flujo python

--- prog1/14-08-2023/flujo.py
+++ prog1/14-08-2023/flujo.py
@@ -1,9 +1,13 @@
 # ejemplo de if-else en python
 edad = int(input("¿Cuántos años tienes?:\n"))
-
+nombre = input("¿Cómo te llamas?:\n")
 if edad < 0:
     print("No puedes tener una edad negativa")
 elif edad < 18 and edad >= 0:
-    print("No puedes usar este programa financiero")
+    if nombre.capitalize() == "Daniel":
+        print("No puedes usar este programa financiero")
+    else:
+        print("Bienvenido, en qué quieres invertir hoy")
 elif edad >= 18:
-    print("Bienvenido, en qué desea invertir hoy")
+    if nombre.capitalize() == "Pepe":
+        print("Hola Admin")
