ec4d51e176eaa4f3aaf3bec5de1a04c374afdc1f
Sebastian Ponce <poncegramming@gmail.com>
2023-08-14 08:28:56 -0600

    control de flujo

---
+++ prog1/14-08-2023/flujo.py
@@ -0,0 +1,7 @@
+# ejemplo de if-else en python
+edad = int(input("¿Cuántos años tienes?:\n"))
+
+if edad < 18:
+    print("No puedes usar este programa financiero")
+elif edad >= 18:
+    print("Bienvenido, en qué desea invertir hoy")
