eaacbbde6c4f364e3adfce057416830d4c76972f
Sebastian Ponce <poncegramming@gmail.com>
2023-10-15 21:29:54 -0600

    movimientos optimos

--- prog1/proyfinallab.py
+++ prog1/proyfinallab.py
@@ -1,5 +1,21 @@
 import os
 import random
+import tkinter as tk
+
+while True:
+    dificultad=(input("Selecciona la difícultad: 1 para fácil, 2 para normal y 3 para difícil: "))
+    dificultad=dificultad.lower()
+    if dificultad=="uno" or dificultad=="1":
+        dificultad=5
+        break
+    elif dificultad=="dos" or dificultad=="2":
+        dificultad=7
+        break
+    elif dificultad=="tres" or dificultad=="3":
+        dificultad=9
+        break
+    else: 
+        print("Selecciona una de las opciones.")
 
 def generate_maze(rows, cols):
     maze = [['#'] * (2 * cols + 1) for _ in range(2 * rows + 1)]
@@ -21,19 +37,7 @@ def generate_maze(rows, cols):
 
     return maze
 
-def print_maze(maze):
-    for row in maze:
-        print(' '.join(row))
-
-def clear_screen():
-    os.system('clear')
-def print_maze_with_player(maze, player_pos):
-    maze_with_player = [row[:] for row in maze]
-    y, x = player_pos
-    maze_with_player[y][x] = 'O'
-    
-    clear_screen()
-    print_maze(maze_with_player)
+import tkinter as tk
 
 def move_player(maze, player_pos, direction):
     y, x = player_pos
@@ -47,24 +51,48 @@ def move_player(maze, player_pos, direction):
         print("Movimiento inválido. No puedes atravesar paredes.")
         return y, x
 
-rows, cols = 5, 5
+def on_key_press(event):
+    global player_pos, move_count
+    direction = event.char.upper()
+    if direction in ('W', 'A', 'S', 'D'):
+        player_pos = move_player(maze, player_pos, DIRECTIONS[direction])
+        move_count += 1
+        print_maze_with_player()
+
+def print_maze_with_player():
+    global window
+    canvas.delete("all")
+    for y, row in enumerate(maze):
+        for x, cell in enumerate(row):
+            color = "white" if cell == ' ' else "black"
+            canvas.create_rectangle(x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE, fill=color, outline=color)
+    y, x = player_pos
+    canvas.create_oval(x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE, fill="red", outline="red")
+
+    if player_pos == (2 * rows, 2 * cols - 1):
+        print("¡Has llegado a la salida!")
+        print(f"Número de movimientos realizados: {move_count}")
+        optimal_moves = abs(player_pos[0] - start_pos[0]) + abs(player_pos[1] - start_pos[1])
+        print(f"Cantidad óptima de movimientos: {int(optimal_moves*1.5)}")
+        window.destroy()
+
+rows, cols = dificultad, dificultad
 maze = generate_maze(rows, cols)
 player_pos = (0, 1)
+start_pos = player_pos
+move_count = 0
 
-while player_pos != (2 * rows, 2 * cols - 1):
-    print_maze_with_player(maze, player_pos)
+DIRECTIONS = {'W': (-1, 0), 'A': (0, -1), 'S': (1, 0), 'D': (0, 1)}
 
-    direction = input("Ingresa la dirección (W/A/S/D): ").upper()
+CELL_SIZE = 40
+window = tk.Tk()
+window.title("LaberinTec")
 
-    if direction == 'W':
-        player_pos = move_player(maze, player_pos, (-1, 0))
-    elif direction == 'A':
-        player_pos = move_player(maze, player_pos, (0, -1))
-    elif direction == 'S':
-        player_pos = move_player(maze, player_pos, (1, 0))
-    elif direction == 'D':
-        player_pos = move_player(maze, player_pos, (0, 1))
-    else:
-        print("Entrada no válida. Utiliza W/A/S/D para moverte.")
+canvas = tk.Canvas(window, width=(cols*2+1) * CELL_SIZE, height=(rows*2+1) * CELL_SIZE, bg="white")
+canvas.pack()
 
-print("¡Felicidades! Has llegado a la salida.")
+window.bind("<Key>", on_key_press)
+
+print_maze_with_player()
+
+window.mainloop()
